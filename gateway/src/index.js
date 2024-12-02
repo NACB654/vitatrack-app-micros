58233c7ca7062a82ea8a21feae34696b676d1c3c
NACB654 <nacb654@gmail.com>
2024-12-02 15:18:11 -0500

    Inicializando gateway

---
+++ gateway/src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+  }),
+);
+
+const PORT = 3010;
