ea7bf8197d1fec1a7dcd635b25ed3be4572d9cef
NACB654 <nacb654@gmail.com>
2024-12-02 19:14:15 -0500

    Gateway API v.1 completo

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -11,3 +11,35 @@ app.use(
 );
 
 const PORT = 3010;
+
+app.get("/gateway/:id", async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const userRequest = axios.post("http://3.139.101.54/profile/me", {
+      userId: id,
+    });
+    const friendsRequest = axios.post("http://18.191.203.53/amistad/amigos", {
+      userId: id,
+    });
+
+    const [userResponse, friendsResponse] = await Promise.all([
+      userRequest,
+      friendsRequest,
+    ]);
+
+    const data = {
+      user: userResponse?.data,
+      friends: friendsResponse?.data,
+    };
+
+    res.status(200).json(data);
+  } catch (err) {
+    console.error("Error", err);
+    res.status(500).json({ msg: err });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Gateway iniciado");
+});
