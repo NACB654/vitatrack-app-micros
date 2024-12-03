1f6b1f1cff5fbc0fa87f6e68d6e02f450f0f6466
NACB654 <nacb654@gmail.com>
2024-12-03 14:27:04 -0500

    Agregando url de habitos

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -19,18 +19,26 @@ app.get("/gateway/:id", async (req, res) => {
     const userRequest = axios.post("http://3.139.101.54/profile/me", {
       userId: id,
     });
-    const friendsRequest = axios.post("http://18.191.203.53/amistad/amigos", {
+    const friendsRequest = axios.post("http://3.20.232.88/amistad/amigos", {
       userId: id,
     });
+    const habitsRequest = axios.post(
+      "http://vitatrack-habitos-lb-21903008.us-east-2.elb.amazonaws.com/habito/hoy",
+      {
+        userId: id,
+      },
+    );
 
-    const [userResponse, friendsResponse] = await Promise.all([
+    const [userResponse, friendsResponse, habitsResponse] = await Promise.all([
       userRequest,
       friendsRequest,
+      habitsRequest,
     ]);
 
     const data = {
       user: userResponse?.data,
       friends: friendsResponse?.data,
+      habits: habitsResponse?.data,
     };
 
     res.status(200).json(data);
